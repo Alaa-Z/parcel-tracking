1256a24f6328e6ee3c6336836d0c0ba2dd11a957
Alaa <alaa.kh.zaza@gmail.com>
2020-11-07 23:36:57 +0100

    fetch api by creating Detail file withe parcel details and show them in Parcel file

---
+++ src/component/molecules/Detail.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {useState} from "react";
+
+import"../../css/detail.css";
+export default function Detail ({data}){
+    const {  
+        id,
+        status,
+        eta,
+        parcel_id,
+        sender,
+        verification_required,
+        location_id,
+        location_name,
+        location_coordinate_latitude,
+        location_coordinate_longitude,
+        location_status_ok,
+        user_phone,
+        user_name,
+        notes,
+        last_updated,
+          } = data;
+
+        const [ openDetails, setOpenDetails] = useState(false); // Initially false
+
+return (
+    <ul className="detail">
+    <li> <b class="first">Parcel id: {parcel_id }</b></li>
+    <li> Estimated time of arrival: {eta}</li>
+    
+            <button
+                onClick={() => setOpenDetails(!openDetails)} // Make it true if false, or false if true
+            >
+                Show me more
+            </button>
+            { openDetails 
+                ? // If true, your code
+                  <p><li> Estimated time of arrival: {eta}</li>
+                     <li> Status: {status} </li>  <li>Sender: {sender}</li>
+                     <li>Verification_required: {verification_required} </li>
+                     <li>Location_name: {location_name}</li>
+                     <li>Last_updated: {last_updated}</li>
+                     </p>
+                 
+                : // If false, Nothing
+                null  
+            }
+    
+    </ul>
+ 
+
+
+);
+
+}
